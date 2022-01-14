13c93abee5645911676863f7e1cc3a74e4d36cf9
junpeko5 <junpeko5@gmail.com>
2022-01-14 23:08:06 +0900

    セレクトボックスに未選択の初期値を追加

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -8,6 +8,7 @@
       v-bind="$attrs"
       class="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
     >
+      <option>選択してください</option>
       <option
         v-for="(option, index) in options"
         :value="option"
